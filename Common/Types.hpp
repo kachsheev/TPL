c3d37c526f01406ca777a78d92d90a8b29286e08
Anton Kashcheev <antonkachsheev@gmail.com>
2016-10-03 08:21:05 +0300

    Added 'links' to primitive types

---
+++ Common/Types.hpp
@@ -0,0 +1,30 @@
+/*
+ * file:       Types.hpp
+ *
+ * created:    02.10.2016
+ * author:     Anton Kashcheev
+ */
+#ifndef TYPES_HPP
+#define TYPES_HPP
+
+#include <cstdint>
+
+namespace tpl
+{namespace types
+{
+
+typedef std::uint8_t uint8_t;
+typedef std::uint16_t uint16_t;
+typedef std::uint32_t uint32_t;
+typedef std::uint64_t uint64_t;
+
+typedef std::int8_t int8_t;
+typedef std::int16_t int16_t;
+typedef std::int32_t int32_t;
+typedef std::int64_t int64_t;
+
+typedef std::size_t size_t;
+
+}}
+
+#endif // TYPES_HPP
