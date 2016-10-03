aad64192a8439463f0b59c2e6059fbf4b0f74750
Anton Kashcheev <antonkachsheev@gmail.com>
2016-10-03 08:21:43 +0300

    Added simple Traits

--- Common/Templates.hpp
+++ Common/Templates.hpp
@@ -8,6 +8,8 @@
 #ifndef COMMON_HPP
 #define COMMON_HPP
 
+#include "Types.hpp"
+
 namespace tpl
 {namespace templates
 {
@@ -15,47 +17,157 @@ namespace tpl
 template<class T>
 struct RemoveReference
 {
-	typedef T type;
+	typedef T Type;
 };
 template<class T>
 struct RemoveReference<T &>
 {
-	typedef T type;
+	typedef T Type;
 };
 template<class T>
 struct RemoveReference<T &&>
 {
-	typedef T type;
+	typedef T Type;
 };
 
-template<class T> inline
-constexpr T&& foward(typename RemoveReference<T>::type& object) noexcept
+template<class T>
+struct Traits: public RemoveReference<T>
+{
+	typedef typename RemoveReference<T>::Type Type;
+	typedef Type & Reference;
+	typedef const Type & ConstReference;
+	typedef Type && LReference;
+	typedef Type * Pointer;
+	typedef const Type * ConstPointer;
+	typedef Type * const PointerConst;
+	typedef const Type * const ConstPointerConst;
+};
+
+template<class T>
+struct Traits<T &>: public RemoveReference<T &>
+{
+	typedef typename RemoveReference<T>::Type Type;
+	typedef Type & Reference;
+	typedef const Type & ConstReference;
+	typedef Type && LReference;
+	typedef Type * Pointer;
+	typedef const Type * ConstPointer;
+	typedef Type * const PointerConst;
+	typedef const Type * const ConstPointerConst;
+};
+
+template<class T>
+struct Traits<const T &>: public RemoveReference<const T &>
+{
+	typedef typename RemoveReference<T>::Type Type;
+	typedef Type & Reference;
+	typedef const Type & ConstReference;
+	typedef Type && LReference;
+	typedef Type * Pointer;
+	typedef const Type * ConstPointer;
+	typedef Type * const PointerConst;
+	typedef const Type * const ConstPointerConst;
+};
+
+template<class T>
+struct Traits<T &&>: public RemoveReference<T &&>
+{
+	typedef typename RemoveReference<T>::Type Type;
+	typedef Type & Reference;
+	typedef const Type & ConstReference;
+	typedef Type && LReference;
+	typedef Type * Pointer;
+	typedef const Type * ConstPointer;
+	typedef Type * const PointerConst;
+	typedef const Type * const ConstPointerConst;
+};
+
+template<class T>
+struct Traits<T *>
+{
+	typedef T Type;
+	typedef Type & Reference;
+	typedef const Type & ConstReference;
+	typedef Type && LReference;
+	typedef Type * Pointer;
+	typedef const Type * ConstPointer;
+	typedef Type * const PointerConst;
+	typedef const Type * const ConstPointerConst;
+};
+
+template<class T>
+struct Traits<const T *>
+{
+	typedef T Type;
+	typedef Type & Reference;
+	typedef const Type & ConstReference;
+	typedef Type && LReference;
+	typedef Type * Pointer;
+	typedef const Type * ConstPointer;
+	typedef Type * const PointerConst;
+	typedef const Type * const ConstPointerConst;
+};
+
+template<class T>
+struct Traits<T * const>
+{
+	typedef T Type;
+	typedef Type & Reference;
+	typedef const Type & ConstReference;
+	typedef Type && LReference;
+	typedef Type * Pointer;
+	typedef const Type * ConstPointer;
+	typedef Type * const PointerConst;
+	typedef const Type * const ConstPointerConst;
+};
+
+template<class T>
+struct Traits<const T * const>
+{
+	typedef T Type;
+	typedef Type & Reference;
+	typedef const Type & ConstReference;
+	typedef Type && LReference;
+	typedef Type * Pointer;
+	typedef const Type * ConstPointer;
+	typedef Type * const PointerConst;
+	typedef const Type * const ConstPointerConst;
+};
+
+template<class T> inline constexpr
+T&& foward(T&& object) noexcept
 {
 	return static_cast<T &&>(object);
 }
 
-template<class T> inline
-constexpr typename RemoveReference<T>::type move(T &&object)
+template<class T> inline constexpr
+typename RemoveReference<T>::Type move(T &&object)
 {
-	return static_cast<typename RemoveReference<T>::type>(object);
+	return static_cast<typename RemoveReference<T>::Type &&>(object);
 }
 
-template<class ReturnType, class ... Args>
-constexpr unsigned int getCountArgs(ReturnType (*)(Args ...))
+template<class ReturnType, class ... Args> constexpr
+types::size_t countArgs(ReturnType (*)(Args ...))
 {
 	return sizeof ... (Args);
 }
 
-template<class Object, class ReturnType, class ... Args>
-constexpr unsigned int getCountArgs(ReturnType (Object::*)(Args ...))
+template<class Object, class ReturnType, class ... Args> constexpr
+types::size_t countArgs(ReturnType (Object::*)(Args ...))
 {
 	return sizeof ... (Args);
 }
 
-template<class ... Args>
-constexpr unsigned int getCountArgs()
+template<class ... Args> constexpr
+types::size_t countArgs()
 {
 	return sizeof ... (Args);
+}
+
+template<typename T> inline constexpr
+const T &constantize(T &&obj) noexcept
+{
+	return static_cast<const T &>(obj);
 }
 
 }}
