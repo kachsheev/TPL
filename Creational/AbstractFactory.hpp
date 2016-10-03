b10873cad7e59ccec4afc8bbf1bb954ca8d7226a
Anton Kashcheev <antonkachsheev@gmail.com>
2016-10-03 08:23:56 +0300

    Generating AbstractFactory using as namespace.

--- Creational/AbstractFactory.hpp
+++ Creational/AbstractFactory.hpp
@@ -8,49 +8,34 @@
 #ifndef CREATIONAL_ABSTRACTFACTORY_HPP
 #define CREATIONAL_ABSTRACTFACTORY_HPP
 
-
-
 // создание общих групп объектов
 
 /*
  * лучше всего сделать
  * на макросах с переменным числом аргументов
  *
- * осталось с ними разобраться
  */
 
 #include "../Common/Preprocessor.hpp"
 #include "../Common/Templates.hpp"
 
-#define tplAbstractFactoryMethod(TYPE_NAME)				\
-static TYPE_NAME *create##TYPE_NAME()					\
-{														\
-	return new TYPE_NAME();								\
-}														\
-														\
+#undef OVERLOADING
+#define OVERLOADING(SUBJ) tplAbstractFactoryMethod(SUBJ)
+
+#define tplAbstractFactoryMethod(TYPE_NAME)			\
 template<class ... Args>								\
-static TYPE_NAME *create##TYPE_NAME(Args&& ... args)		\
+TYPE_NAME *create##TYPE_NAME(Args &&... args)	\
 {														\
 	return new TYPE_NAME(templates::foward(args) ...);	\
 }
 
-#define OVERLOADING(SUBJ) tplAbstractFactoryMethod(SUBJ)
-
-#define tplAbstractFactory(FACTORY_NAME, ...)			\
-namespace tpl											\
-{														\
-class FACTORY_NAME										\
-{														\
-public:													\
-	FACTORY_NAME() = delete;							\
-	FACTORY_NAME(FACTORY_NAME &&factory) = delete;		\
-	FACTORY_NAME(const FACTORY_NAME &factory);			\
-														\
-	operator=(FACTORY_NAME &&factory) = delete;			\
-	operator=(const FACTORY_NAME &factory) = delete;	\
-														\
-	tplGenerator(__VA_ARGS__)							\
-};														\
+#define tplAbstractFactory(FACTORY_NAME, ...)						\
+namespace tpl														\
+{																	\
+namespace FACTORY_NAME												\
+{																	\
+	tplGenerator(__VA_ARGS__)										\
+}																	\
 }
 
 #endif // CREATIONAL_ABSTRACTFACTORY_HPP
